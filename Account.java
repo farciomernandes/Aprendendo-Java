e1955133d01b306869a372ebdd7b30406adf2eab
Marcio Fernandes Dos Santos <farciomernandes@gmail.com>
2020-03-30 10:12:59 -0300

    Primeiro Programinha

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account{
+
+    private String name; //Variavél de instancia
+
+    public void setName(String name){ //Método para definir o nome do objeto
+        this.name = name; //Armazena o nome
+    }
+
+        public String getName(){ //Método que recupera o nome do objeto
+            return name;
+        }
+}
---
+++ AccountTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class AccountTest{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        Account myAccount = new Account(); //Cria um objeto Account
+
+        //Exibe o nome inicial = null
+        System.out.printf("O nome inicial é: %s%n%n", myAccount.getName());
+
+        System.out.printf("Digite o seu nome: ");
+        String theName = input.nextLine(); //Lê uma linha de texto
+        myAccount.setName(theName); //Insere theName para myAccount método setName
+        System.out.println(); //Gera uma saída de uma linha em branco
+        //Exibe o nome armazenado no objeto;
+        System.out.printf("O nome do objeto myAccount é: %n%s%n ",
+        myAccount.getName());
+    }
+}
